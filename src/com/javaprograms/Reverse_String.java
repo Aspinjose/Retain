d797597eab8b479a57ab3bd4a9027ab52b115cdf
aspin <aspin@Aspin>
2023-12-21 01:42:15 +0530

    test

---
+++ src/com/javaprograms/Reverse_String.java
@@ -0,0 +1,23 @@
+package com.javaprograms;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+
+public class Reverse_String {
+
+	public static void main(String[] args) {
+
+	String name="Nivetha";	
+			
+StringBuffer buffer=new StringBuffer();
+buffer.append(name);
+System.out.println(buffer.reverse());
+	}
+	/*public void selection() {
+		Toolkit.getDefaultToolkit().getSystemClipboard().getContents()*/
+
+	}
+	
+
+
+for(int i=0,i>=,i++)
